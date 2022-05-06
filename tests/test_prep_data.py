ddca8d317aa3c4d0a2e1418d3af3d869f91555ce
olaaflo <ola.flo@hotmail.com>
2022-05-07 00:33:23 +0200

    cleaned prep data

--- tests/test_prep_data.py
+++ tests/test_prep_data.py
@@ -1,21 +1,21 @@
 import pytest
 import sys
-import os 
+import os
 import shutil
-sys.path.append('code')
+
+sys.path.append("code")
 from prep_data_functions import *
 
 
 @pytest.mark.download_dataset
 def test_download_dataset(tmp_path):
     data_dir = tmp_path
-    dataset_name = 'spaceship-titanic' 
+    dataset_name = "spaceship-titanic"
 
     download_dataset(data_dir, dataset_name)
-    data_set_path = os.path.join(data_dir,dataset_name)
+    data_set_path = os.path.join(data_dir, dataset_name)
 
     assert os.path.isdir(data_set_path) == True
-
 
 
 @pytest.mark.work_space
@@ -29,10 +29,8 @@ def test_create_work_space(tmp_path):
     create_dirs = ["annotations", "images", "training_data"]
     for dir in create_dirs:
         created_paths.append(os.path.join(train_dir, dir))
-    
+
     create_work_space(data_dir)
 
     for dir in created_paths:
         assert os.path.isdir(dir) == True
-    
-
